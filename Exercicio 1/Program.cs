edefae870e22703bb30d32a36971b461d6932b77
Sylmara <sylmara.caldas@gmail.com>
2023-01-16 00:12:11 -0300

    retirada comentários ex1

--- Exercicio 1/Program.cs
+++ Exercicio 1/Program.cs
@@ -8,13 +8,11 @@
             // 1. instânciando pessoa
             Pessoa pessoa = new Pessoa();
 
-            // coletando os dados
             Console.WriteLine("Digite seu nome: ");
             pessoa.Nome = Console.ReadLine(); 
             bool check;
             int idade;
 
-            //verificando a entrada
             do
             {
                 Console.WriteLine("Digite sua idade: ");
@@ -22,18 +20,15 @@
             } while (!check);
             pessoa.Idade = idade;
 
-            // chamando os métodos
             pessoa.Cumprimentar();
             pessoa.DizerIdade();
 
             // 2. instânciando aluno
             Aluno aluno = new Aluno();
 
-            // inserindo dados
             aluno.Nome = "Sylmara";
             aluno.Idade = 21;
 
-            // chamando os métodos
             aluno.Cumprimentar();
             aluno.DizerIdade();
             aluno.IrParaEscola();
@@ -42,11 +37,9 @@
             // 3. instânciando professor
             Professor professor = new Professor();
 
-            // inserindo dados
             professor.Nome = "Michael";
             professor.Idade = 30;
 
-            // chamando os métodos 
             professor.Cumprimentar();
             professor.DizerIdade();
             professor.Explicar("Abstração");
