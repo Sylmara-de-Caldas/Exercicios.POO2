edae49c967e894cfb2c9798b6162bb93d09d4d1e
Sylmara <sylmara.caldas@gmail.com>
2023-01-28 15:09:33 -0300

    Exericicio 5

---
+++ Exercicio5/Dicionario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    public class Dicionario
+    {
+        public string Termo { get; set; }
+        public string Traducao { get; set; }
+
+        public Dictionary<string, string> DicionarPadrao()
+        {
+            Dictionary<string, string> dicionario = new();
+
+            dicionario["Get up"] = "levantar; acordar";
+            dicionario["Give up"] = "desistir";
+            dicionario["Give out"] = "distribuir; chegar ao fim; parar de funcionar";
+            dicionario["Go after"] = "ir atrás, perseguir";
+            dicionario["Look into"] = "investigar, examinar, analisar";
+            dicionario["Make up"] = "inventar, criar (história, explicação); maquiar; fazer as pazes";
+            dicionario["Take apart"] = "desmontar";
+            dicionario["Take over"] = "assumir o controle (de algo)";
+            dicionario["Turn down"] = "trejeitar, recusar; abaixar";
+            dicionario["Get out of hand "] = "perder o controle";
+            dicionario["Miss the boat"] = "ser tarde demais";
+
+            return dicionario;
+        }
+        public void VerificarTermo(Dictionary<string, string> dicionario)
+        {
+            Console.WriteLine("Digite o termo que deseja verificar: ");
+
+            string termo = Console.ReadLine();
+
+            string[] termos = termo.Split(" ");
+
+            bool check;
+
+            string resposta = string.Empty;
+
+            foreach (string term in termos)
+            {
+                check = dicionario.TryGetValue(term, out string traducao);
+
+            }
+
+            Console.WriteLine($"O significado do termo {termo} é: {resposta.Trim()}.");
+
+        }
+        public void AdicionarTermo()
+        {
+            Console.WriteLine("Digite quantos termos deseja adicionar: ");
+            int qtde = int.Parse(Console.ReadLine());
+
+            Dicionario[] vetor = new Dicionario[qtde];
+
+            for (int i = 0; i < qtde; i++)
+            {
+                string termo = Console.ReadLine();
+                string traducao = Console.ReadLine();
+                vetor[i] = new Dicionario { Termo = termo, Traducao = traducao };
+                Console.Write("Adição concluída: ");
+                Console.WriteLine($"'{termo}' = '{traducao}' ");
+            }
+
+        }
+    }
+}
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dicionario dicionario = new Dicionario();
+
+            Console.WriteLine("Bem vindo!!");
+            Console.WriteLine("Dicionário -  Let’s Speak!");
+
+            Console.WriteLine("\nO que deseja fazer?");
+            Console.WriteLine("\n[1] - Consultar termo");
+            Console.WriteLine("[2] - Adicionar termo");
+            Console.WriteLine("[3] - Sair\n");
+            string input = Console.ReadLine();
+
+            do
+            {
+                switch (input)
+                {
+                    case "1":
+                        dicionario.VerificarTermo(dicionario);
+                        break;
+                    case "2":
+                        dicionario.AdicionarTermo();
+                        break;
+                }
+            } while (input != "3");
+           
+
+
+
+
+
+        }
+
+
+    }
+}
